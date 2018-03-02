91d8cf9e1eb252e0f833c082ad70cf1aedf1669e
daniel.go.mon <daniel.go.mon@gmail.com>
2018-03-02 03:05:12 -0600

    Works with follow-ups
    userId added to make it work with follow-ups responses

--- DialogFuel.php
+++ DialogFuel.php
@@ -8,10 +8,12 @@ header('Content-Type: application/json;charset=utf-8');
 require_once dirname(__FILE__) . '/unirest-php/src/Unirest.php';
 require_once dirname(__FILE__) . '/Config.php';
 
+
 //==================== Connect To Dialog Flow ==================
 $txttodialogflow = $_GET['txttodialogflow'];
 $headers = array("contentType"=>"application/json; charset=utf-8","Authorization"=>"Bearer ".$ClientToken);
-$url = "https://api.dialogflow.com/v1/query?v=" . $qversion.'&query='.$txttodialogflow.'&sessionId=Form PHP-DialogFuel'.'&lang='.$lang.'';
+$sessionId = $_GET['fb_id'];
+$url = "https://api.dialogflow.com/v1/query?v=" . $qversion. '&query=' . $txttodialogflow . '&sessionId=' . $sessionId . '&lang=' . $lang . '';
 $response = Unirest\Request::get($url, $headers);
 
 
@@ -19,7 +21,6 @@ $response = Unirest\Request::get($url, $headers);
 $response_body = json_decode($response->raw_body,true);
 $getword = $response_body['result']['fulfillment']['speech'];
 $getblock = $response_body['result']['fulfillment']['messages']['0']['payload']['redirect_to_blocks']; 
-
 
 if($getword !=null && $getword !='')
 {
@@ -42,11 +43,12 @@ else
 	$messages = array(
 		'messages' => array(
 			0 => array(
-				'text' => 'Sorry, there is an error.',		// Remove 'Sorry,Chatbot is Error.' if you Don't want to Show Error Message.
+				'text' => 'Sorry, there is an error.',		// Remove 'Sorry, there is an error.' if you don't want to show an error message.
 			),
 		),
 	);
 }
+
 
 //================== Send to Chatfuel Block ==================
 $return_messages = json_encode($messages);
